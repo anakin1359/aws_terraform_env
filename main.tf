558ccceb8e9c8394e846f08c9d943fb9763eeef5
anakin1359 <anakin1359@gmail.com>
2022-04-30 03:35:01 +0900

    terraform exec env setup

---
+++ main.tf
@@ -0,0 +1,31 @@
+# ---------------------------
+# Terraform Configure
+# ---------------------------
+terraform {
+    required_version = ">=0.13"   # Specify version 0.13 or higher
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws" # Specify the module name
+            version = "~>3.0"         # 3.0 or higher (ignore minor versions)
+        }
+    }
+}
+
+# ---------------------------
+# Provider
+# ---------------------------
+provider "aws" {
+    profile = "terraform"
+    region  = "ap-northeast-1"
+}
+
+# ---------------------------
+# Variables
+# ---------------------------
+variable "project" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
---
+++ terraform_cli_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://www.terraform.io/downloads
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+sleep 3s
+terraform -version
