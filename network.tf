c98dbcf28feb3d70c3fdb7f55e05d78cd9918e68
anakin1359 <anakin1359@gmail.com>
2022-04-30 05:28:07 +0900

    setting subnet

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 # ---------------------------
 # VPC
 # ---------------------------
-resource "aws_vpc" "vpc" {
-    cidr_block                       = "192.168.1.0/24"
+resource "aws_vpc" "main" {
+    cidr_block                       = "192.168.0.0/20"
     instance_tenancy                 = "default"
     enable_dns_support               = true
     enable_dns_hostnames             = true
@@ -14,3 +14,34 @@ resource "aws_vpc" "vpc" {
         Env     = var.environment
     }
 }
+
+# ---------------------------
+# Subnet (Public)
+# ---------------------------
+resource "aws_subnet" "public-subnet-1a" {
+    vpc_id                  = aws_vpc.main.id
+    availability_zone       = "ap-northeast-1a"
+    cidr_block              = "192.168.1.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name    = "${var.project}-${var.environment}-public-subnet-1a"
+        Project = var.project
+        Env     = var.environment
+        Type    = "public"
+    }
+}
+
+# resource "aws_subnet" "public-subnet-1c" {
+#     vpc_id                  = aws_vpc.main.id
+#     availability_zone       = "ap-northeast-1c"
+#     cidr_block              = "192.168.2.0/24"
+#     map_public_ip_on_launch = true
+
+#     tags = {
+#         Name    = "${var.project}-${var.environment}-public-subnet-1c"
+#         Project = var.project
+#         Env     = var.environment
+#         Type    = "public"
+#     }
+# }
