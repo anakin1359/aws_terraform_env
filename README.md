1f95d868df9c66ef2eea0a46d95e08cbd6178e5a
anakin1359 <anakin1359@gmail.com>
2022-05-01 01:57:50 +0900

    edit README.md

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ secret_key = "SECRET_KEY"
 region     = "REGION"
 zone       = "ZONE"
 ```
+<br>
+
+### Note: Always run "terraform init" if data.tf or config.tfvars is changed.
+```
+terraform init
+```
+<br>
+
+### Always run "plan" when creating, modifying, or deleting resources.
+### When executing "plan" and "apply", refer to the variables in "config.tfvars".
+```
+terraform plan --var-file=./config.tfvars
+```
+```
+terraform apply --var-file=./config.tfvars -auto-approve
+```
--- ec2.tf
+++ ec2.tf
@@ -15,26 +15,26 @@ resource "aws_key_pair" "ssh_keypair" {
 # ---------------------------
 # EC2 Instance
 # ---------------------------
-resource "aws_instance" "app_cassis" {
-    # setting resource
-    ami           = data.aws_ami.ubuntu-1804.id
-    instance_type = "t2.micro"
+# resource "aws_instance" "app_cassis" {
+#     # setting resource
+#     ami           = data.aws_ami.ubuntu-1804.id
+#     instance_type = "t2.micro"
 
-    # binf subnet-address
-    subnet_id                   = aws_subnet.public_subnet_1a.id
-    associate_public_ip_address = true                            # Whether to associate a public IP address with an instance in a VPC.
+#     # binf subnet-address
+#     subnet_id                   = aws_subnet.public_subnet_1a.id
+#     associate_public_ip_address = true                            # Whether to associate a public IP address with an instance in a VPC.
 
-    # bind secrity-group
-    vpc_security_group_ids = [
-        aws_security_group.web_security_group.id
-    ]
+#     # bind secrity-group
+#     vpc_security_group_ids = [
+#         aws_security_group.web_security_group.id
+#     ]
 
-    # bind ssh_keypair
-    key_name = aws_key_pair.ssh_keypair.key_name
+#     # bind ssh_keypair
+#     key_name = aws_key_pair.ssh_keypair.key_name
 
-    tags = {
-        Name    = "${var.project}_${var.environment}_app_cassis"
-        Project = var.project
-        Env     = var.environment
-    }
-}
+#     tags = {
+#         Name    = "${var.project}_${var.environment}_app_cassis"
+#         Project = var.project
+#         Env     = var.environment
+#     }
+# }
